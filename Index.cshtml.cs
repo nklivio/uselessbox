855241610eb17845aff4d585c07d86e04a72f1c5
nklivio <nklivio@yahoo.com>
2024-02-28 09:55:00 -0800

    Add files via upload

---
+++ Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace YourNamespace.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
